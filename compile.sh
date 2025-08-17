05add5a2dde354363c312ef5689ee213b952ba03
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-08-17 04:54:52 +0200

    Add compile script

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+TEXFILE="main"
+OUTDIR="out"
+
+mkdir -p "$OUTDIR"
+
+lualatex -interaction=nonstopmode -output-directory="$OUTDIR" "$TEXFILE.tex"
+
+biber --input-directory "$OUTDIR" --output-directory "$OUTDIR" "$TEXFILE"
+
+makeindex -s index.ist "$OUTDIR/$TEXFILE.idx"
+
+lualatex -interaction=nonstopmode -output-directory="$OUTDIR" "$TEXFILE.tex"
+
+echo "Compilation finished."
